6a588eef90b436e29f84a02a690269c7ed626a9b
jatinscodingproject <jatincoder104@gmail.com>
2026-04-05 11:03:42 -0700

    code

--- Services/browsermanager.js
+++ Services/browsermanager.js
@@ -1,27 +1,45 @@
+  // GNU nano 7.2                                                                                                                  browsermanager.js
 const puppeteer = require("puppeteer");
+const os = require("os");
 
 let browser = null;
 
 async function getBrowser() {
   if (browser) return browser;
 
+  const isLinux = os.platform() === "linux";
+  const isWindows = os.platform() === "win32";
+
+  let executablePath;
+  let userDataDir;
+
+  if (isLinux) {
+    // Linux paths (your server)
+    executablePath = "/usr/bin/chromium-browser";
+    userDataDir = "/root/puppeteer/chrome-profile";
+  } else if (isWindows) {
+    // Windows paths (local dev)
+    executablePath =
+      "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe";
+    userDataDir = "C:\\puppeteer\\chrome-profile";
+  }
+
   browser = await puppeteer.launch({
-    headless: false,
+    headless: isLinux ? "new" : false,
 
-    executablePath:
-      "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe",
-
-    userDataDir: "C:\\puppeteer\\chrome-profile",
+    executablePath,
+    userDataDir,
 
     ignoreHTTPSErrors: true,
 
     args: [
       "--no-sandbox",
       "--disable-setuid-sandbox",
+      "--disable-dev-shm-usage",
+      "--disable-infobars",
       "--disable-features=HttpsFirstMode",
       "--disable-web-security",
-      "--disable-infobars",
-      "--start-maximized",
+      ...(isLinux ? [] : ["--start-maximized"]),
     ],
 
     ignoreDefaultArgs: ["--enable-automation"],
@@ -40,4 +58,3 @@ async function closeBrowser() {
 }
 
 module.exports = { getBrowser, closeBrowser };
-
